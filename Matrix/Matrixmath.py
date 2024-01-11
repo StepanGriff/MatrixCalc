5f20af560a9913a4a9573d400b910e929637a62e
StepanGriff <stevengriffintech@gmail.com>
2024-01-11 16:18:42 -0500

    determinant commit

--- Matrix/Matrixmath.py
+++ Matrix/Matrixmath.py
@@ -45,8 +45,7 @@ class MatrixMath:
         assert len(x) == len(x[0]), "Matrix is not square"
         det=0
         alt = 1
-        matrix =list[list[int]]
-        if len(x) ==2:
+        if len(x) == 2:
             det = (x[0][0]*x[1][1] - x[1][0]*x[0][1])
             return (det)
         else:
@@ -55,18 +54,27 @@ class MatrixMath:
                     alt = 1
                 else:
                     alt =-1 
-                det += (alt*x[0][num])*det(self.matrixOfMinors(x, num))
+                det += (alt*x[0][num])*self.determinant(self.matrixOfMinors(x, num))
             return (det)
 
 
     def matrixOfMinors(self, x, y:int):
         minor = list[list[int]]
-        x.remove
-        x.pop(0)
-        for num in range(len(x)):
-            x[num].pop(y)
-        minor = x
+        z = self.cloneMatrix(x)
+        z.remove
+        z.pop(0)
+        for num in range(len(z)):
+            z[num].pop(y)
+        minor = z
         return minor
+    
+    def cloneMatrix(self, x):
+        clone= [[0 for val in range(len(x))] for val2 in range(len(x))]
+        for num in range(len(x)):
+            for num2 in range(len(x[0])):
+                clone[num][num2] = x[num][num2]
+        return clone
+
 
     def division(self):
         self.b = self.invert(self.b)
@@ -93,6 +101,6 @@ x = MatrixMath([[1,2],[3,4]],[[5,6],[7,8]])
 #print (x.subtract())
 #print (x.multiplication())
 #print (x.division())
-z = [[0,1,2],[3,4,5],[6,7,8]]
+z = [[1,2,3,15],[4,5,6,16],[7,8,12,19],[45,16,12,17]]
 print(x.determinant(z))
 #print(x.matrixOfMinors(z,1))
